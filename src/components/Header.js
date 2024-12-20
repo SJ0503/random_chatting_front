92eb99e0b4fa341268afd6c5cf919a817ca37797
SJ0503 <didtpwls3857@gmail.com>
2024-12-20 21:29:43 +0900

    커밋

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header style={{ backgroundColor: "#f8f8f8", padding: "10px" }}>
+            <h1>Chatting Rooms</h1>
+            
+        </header>
+    );
+}
+
+export default Header;
